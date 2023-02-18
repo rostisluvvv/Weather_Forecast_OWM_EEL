2e463c80219a98787082fd713094cf6c694ccf85
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-02-18 14:14:54 +0300

    the project structure has been changed

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from backend.weather import *
 
 if __name__ == '__main__':
     eel.init('frontend')
-    eel.start('index.html', mode="chrome", size=(760, 760))
+    eel.start('templates/index.html', mode="chrome", size=(760, 760))
